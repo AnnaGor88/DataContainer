189866af26b6551609846d16c711432f0f47c0f1
Anna <goryachevaanny@gmail.com>
2021-04-26 20:47:00 +0300

    Initial Commit

---
+++ DataContainer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataContainer
+//
+//  Created by Анна Горячева on 26.04.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
